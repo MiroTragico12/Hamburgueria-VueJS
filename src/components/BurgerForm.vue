60662c1c2ff11010d9918ba316e578c847bb4d15
MiroTragico12 <jorge.santista10@hotmail.com>
2024-06-12 12:28:33 -0300

    commit

--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -33,142 +33,141 @@
       </form>
     </div>
   </template>
-
-<script>
-
-
-    export default{
-        name:"BurgerForm",
-        data(){
-            return{
-                paes:null,
-                carnes:null,
-                opcionaisdata:null,
-                nome:null,
-                pao:null,
-                carne:null,
-                opcionais:[],
-                msg:null
-            }
-        },
-        methods:{
-            async getIngredients(){
-                const req = await fetch("http://localhost:3000/ingredientes");
-                const data = await req.json();
-
-                this.paes = data.paes;
-                this.carnes = data.carnes;
-                this.opcionaisdata = data.opcionais;
-            }
-        },
-        mounted(){
-            this.getIngredients();
-        },
-        async createBurger(e){
-            e.preventDefault()
-
-const data = {
-  nome: this.nome,
-  carne: this.carne,
-  pao: this.pao,
-  opcionais: Array.from(this.opcionais),
-  status: "Solicitado"
-}
-
-const dataJson = JSON.stringify(data)    
-
-const req = await fetch("http://localhost:3000/burgers", {
-  method: "POST",
-  headers: { "Content-Type" : "application/json" },
-  body: dataJson
-});
-
-const res = await req.json()
-
-console.log(res)
-
-this.msg = "Pedido realizado com sucesso!"
-
-      this.nome = "";
-      this.carne = "";
-      this.pao = "";
-      this.opcionais = "";
-
+  
+  <script>
+    import Message from '/'
+  export default {
+    name: "BurgerForm",
+    data() {
+      return {
+        paes: null,
+        carnes: null,
+        opcionaisdata: null,
+        nome: null,
+        pao: null,
+        carne: null,
+        opcionais: [],
+        status: "Solicitado",
+        msg: null
+      }
+    },
+    methods: {
+      async getIngredientes() {
+        const req = await fetch('http://localhost:3000/ingredientes')
+        const data = await req.json()
+  
+        this.paes = data.paes
+        this.carnes = data.carnes
+        this.opcionaisdata = data.opcionais
+      },
+      async createBurger(e) {
+  
+        e.preventDefault()
+  
+        const data = {
+          nome: this.nome,
+          carne: this.carne,
+          pao: this.pao,
+          opcionais: Array.from(this.opcionais),
+          status: "Solicitado"
         }
+  
+        const dataJson = JSON.stringify(data)    
+  
+        const req = await fetch("http://localhost:3000/burgers", {
+          method: "POST",
+          headers: { "Content-Type" : "application/json" },
+          body: dataJson
+        });
+  
+        const res = await req.json()
+  
+        console.log(res)
+  
+        
+  
+        // limpar campos
+        this.nome = ""
+        this.carne = ""
+        this.pao = ""
+        this.opcionais = []
+        
+      }
+    },
+    mounted () {
+      this.getIngredientes()
+    },
+    components:{
+        Message
     }
-    
-
-</script>
-
-
-<style scoped>
-  #burger-form {
-    max-width: 400px;
-    margin: 0 auto;
   }
-
-  .input-container {
-    display: flex;
-    flex-direction: column;
-    margin-bottom: 20px;
-  }
-
-  label {
-    font-weight: bold;
-    margin-bottom: 15px;
-    color: #222;;
-    padding: 5px 10px;
-    border-left: 4px solid #fcba03;
-  }
-
-  input, select {
-    padding: 5px 10px;
-    width: 300px;
-  }
-
-  #opcionais-container {
-    flex-direction: row;
-    flex-wrap: wrap;
-  }
-
-  #opcionais-title {
-    width: 100%;
-  }
-
-  .checkbox-container {
-    display: flex;
-    align-items: flex-start;
-    width: 50%;
-    margin-bottom: 20px;
-  }
-
-  .checkbox-container span,
-  .checkbox-container input {
-    width: auto;
-  }
-
-  .checkbox-container span {
-    margin-left: 6px;
-    font-weight: bold;
-  }
-
-  .submit-btn {
-    background-color: #222;
-    color:#fcba03;
-    font-weight: bold;
-    border: 2px solid #222;
-    padding: 10px;
-    font-size: 16px;
-    margin: 0 auto;
-    cursor: pointer;
-    transition: .5s;
-   width: 170px;
-   border-radius: 30px;
-   margin-right: 300px
-  }
-
-  .submit-btn:hover {
-    background-color: transparent;
-    color: #222;
-  }
-</style>
+  </script>
+  
+  <style scoped>
+    #burger-form {
+      max-width: 400px;
+      margin: 0 auto;
+    }
+  
+    .input-container {
+      display: flex;
+      flex-direction: column;
+      margin-bottom: 20px;
+    }
+  
+    label {
+      font-weight: bold;
+      margin-bottom: 15px;
+      color: #222;;
+      padding: 5px 10px;
+      border-left: 4px solid #fcba03;
+    }
+  
+    input, select {
+      padding: 5px 10px;
+      width: 300px;
+    }
+  
+    #opcionais-container {
+      flex-direction: row;
+      flex-wrap: wrap;
+    }
+  
+    #opcionais-title {
+      width: 100%;
+    }
+  
+    .checkbox-container {
+      display: flex;
+      align-items: flex-start;
+      width: 50%;
+      margin-bottom: 20px;
+    }
+  
+    .checkbox-container span,
+    .checkbox-container input {
+      width: auto;
+    }
+  
+    .checkbox-container span {
+      margin-left: 6px;
+      font-weight: bold;
+    }
+  
+    .submit-btn {
+      background-color: #222;
+      color:#fcba03;
+      font-weight: bold;
+      border: 2px solid #222;
+      padding: 10px;
+      font-size: 16px;
+      margin: 0 auto;
+      cursor: pointer;
+      transition: .5s;
+    }
+  
+    .submit-btn:hover {
+      background-color: transparent;
+      color: #222;
+    }
+  </style>
---
+++ src/components/Message.vue
@@ -0,0 +1,16 @@
+<template>
+
+
+</template>
+
+
+<script>
+
+expor default{
+    name:"Message"
+}
+
+</script>
+
+
+<style scoped></style>
